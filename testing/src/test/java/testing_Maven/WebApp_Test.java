b17e3dea6b5eb7313cd37de968964152679a62d2
manzoor <manzoor.mehadi@niit.com>
2021-10-21 23:34:00 +0530

    forth commit

--- testing/src/test/java/testing_Maven/WebApp_Test.java
+++ testing/src/test/java/testing_Maven/WebApp_Test.java
@@ -3,6 +3,7 @@ package testing_Maven;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -27,8 +28,8 @@ public class WebApp_Test {
 	@BeforeClass
 	public void launchApp() {
 		//System.setProperty("webdriver.chrome.driver", "/home/manzoormehadi/Public/Selenium Learning/Jar Files/chromedriver_linux64/chromedriver");
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		driver.get("http://demowebshop.tricentis.com/");
 		driver.manage().window().maximize();
 		System.out.println("application is launched");
